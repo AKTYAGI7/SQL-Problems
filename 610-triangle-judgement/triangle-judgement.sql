c6d82ac805b7f8caca9d585fd6b6692012742ad5
AKTYAGI7 <134074147+AKTYAGI7@users.noreply.github.com>
2024-04-25 15:47:55 +0530

    Time: 667 ms (21.69%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,1 +1,2 @@
-select x,y,z, case when x+y>z and y+z>x and z+x>y then 'Yes' else 'No' end as triangle from triangle;
+select x,y,z, case when x+y>z and y+z>x and z+x>y then 'Yes' else 'No' end as triangle 
+from triangle;
